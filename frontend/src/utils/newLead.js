2928663220342ea3223642dbaa512dee2c3ed2a4
Gurnav224 <chaudharyg856@gmail.com>
2025-01-31 18:57:30 +0530

    basic utilty file function new lead constant and filter by status

---
+++ frontend/src/utils/newLead.js
@@ -0,0 +1,35 @@
+
+
+export const leadSource = [
+    "Website",
+    "Referral",
+    "Cold Call",
+    "Advertisement",
+    "Email",
+    "Other",
+  ];
+
+
+  export   const leadStatus = [
+    "New",
+    "Contacted",
+    "Qualified",
+    "Proposal Sent",
+    "Close",
+  ];
+
+  export const crmTags = [
+    "FollowUp",
+    "HighValue",
+    "Leads",
+    "SalesPipeline",
+    "LeadTracking",
+    "EmailFollowUp",
+    "HotLeads",
+    "ColdLeads",
+    "PriorityClients",
+    "AutomatedReminders",
+    "LeadNurturing",
+  ];
+
+  export const priority = ["High", "Medium", "Low"];
